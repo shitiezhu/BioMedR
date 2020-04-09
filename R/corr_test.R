34d505a969230deb8359f21243d2ce222f5c9c88
shitiezhu <shitiezhu1991@163.com>
2020-04-09 09:21:41 +0800

    corr_test

---
+++ R/corr_test.R
@@ -0,0 +1,10 @@
+
+# for batched cor.test
+
+corr_test <- function(data = NULL, gene = NULL, method = c("pearson", "kendall", "spearman")){
+  y <- as.numeric(data[gene,])
+  corlist <- apply(data, 1, function(x){
+    cor.test(as.numeric(x), y, method = method)
+  })
+  purrr::map_dfr(corlist, broom::tidy, .id = 'mRMA')
+}
