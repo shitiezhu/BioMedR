d4f80ef1ec4fc8b814b2beec26e76b81e18dce56
shitiezhu <shitiezhu1991.com>
2022-07-28 15:39:40 +0800

    qPCR

---
+++ R/qPCR.R
@@ -0,0 +1,28 @@
+qPCR <- function(data = data, Samples = Samples){
+
+  suppressMessages(library(tidyverse), library(ggpubr))
+  data_a <- data %>%
+    full_join({group_by(.,Sample.Name) %>%
+        summarise_at("Ctrl.CT", ~ mean(.x, na.rm = T))}, by = "Sample.Name", suffix=c("",".mean")) %>%
+    mutate(d_CT = CT - Ctrl.CT.mean) %>%
+    full_join({filter(.,Sample.Name == "shNT") %>% group_by(Target.Name) %>%
+        summarise_at("d_CT", ~ mean(.x, na.rm = T))}, by = "Target.Name", suffix=c("",".mean")) %>%
+    mutate(dd_CT = d_CT - d_CT.mean) %>%
+    mutate(`2^-dd_CT` = 2^-(dd_CT)) %>%
+    mutate(Sample.Name = ordered(Sample.Name, levels = Samples))
+
+
+
+  # install.packages("ggpubr")
+  ggbarplot(data_a, x = "Target.Name", y = "2^-dd_CT", add = "mean_se",
+            color = "Sample.Name", palette = "jco", fill = "Sample.Name",
+            position = position_dodge(0.8))+
+    stat_compare_means(aes(group = Sample.Name), label = "p.format") +
+    labs(y = "Rel. mRNA expression", x = "")+
+    facet_wrap(~Target.Name, scales = "free")
+
+  if(!dir.exists("results")) {dir.create("results")}
+  write.csv(data_a, "results/qPCR data ready for analysis.csv", row.names = FALSE)
+
+  ggsave("results/barplot.tiff",width = 8,height = 6)
+}
