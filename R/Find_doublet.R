3992a5406db9d90b377f8d1ca428a6f1e9735feb
shitiezhu <shitiezhu1991@163.com>
2026-01-15 11:26:55 +0800

    reuse.pANN = FALSE removed

--- R/Find_doublet.R
+++ R/Find_doublet.R
@@ -6,7 +6,7 @@ Find_doublet <- function(data){
   bcmvn <- DoubletFinder::find.pK(sweep.stats)
   nExp_poi <- round(0.05*ncol(data))
   p<-as.numeric(as.vector(bcmvn[bcmvn$MeanBC==max(bcmvn$MeanBC),]$pK))
-  data <- DoubletFinder::doubletFinder(data, PCs = 1:20, pN = 0.25, pK = p, nExp = nExp_poi, reuse.pANN = FALSE, sct = FALSE)
+  data <- DoubletFinder::doubletFinder(data, PCs = 1:20, pN = 0.25, pK = p, nExp = nExp_poi, sct = FALSE)
   colnames(data@meta.data)[ncol(data@meta.data)] = "doublet_info"
   data = subset(data, subset = doublet_info == "Singlet")
   return(data)
